98965f80dd8a50985a4062b2b524cf282a0356c1
Benjamin Dzandu <kojo.atieku@gmail.com>
2020-12-08 20:23:18 +0100

    New/Create actions added

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,5 +1,25 @@
 class PicsController < ApplicationController
 
   def index
+    @pics = Pic.all
+  end
+
+  def new
+    @pic = Pic.new
+  end
+
+  def create
+    @pic = Pic.new(pic_params)
+    if @pic.save
+      redirect_to pic_path(@pic)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def pic_params
+    params.require(:pic).permit(:title, :description)
   end
 end
