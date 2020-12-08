06b5b0304aec137d6d7e1fcfe89f7c880d44f7a4
Benjamin Dzandu <kojo.atieku@gmail.com>
2020-12-08 21:00:25 +0100

    Edit/Destroy actions added

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,7 +1,7 @@
 class PicsController < ApplicationController
   before_action :find_pic, only:[:show, :edit, :update, :destroy]
   def index
-    @pics = Pic.all
+    @pics = Pic.all.order("created_at DESC")
   end
 
   def new
@@ -11,13 +11,29 @@ class PicsController < ApplicationController
   def create
     @pic = Pic.new(pic_params)
     if @pic.save
-      redirect_to pic_path(@pic)
+      redirect_to pic_path(@pic), notice: "Picture was created"
     else
       render 'new'
     end
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @pic.update(pic_params)
+      redirect_to pic_path(@pic), notice: "Picture was updated"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pic.destroy
+    redirect_to pics_path
   end
 
   private
